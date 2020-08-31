b664fbdb8cb63e9415b9f02a3a037d336051e01c
TiagoDiass <tiago.costadiasss@gmail.com>
2020-08-31 15:31:14 -0300

    base layout done

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <h2 class="logo">
+      <span class="text-orange">Vue</span><span class="font-weight-light">Trello</span>
+    </h2>
+
+    <nav>
+      <router-link tag="a" to="/boards">
+        <i class="fas fa-clipboard-list"></i>
+        Seus Quadros
+      </router-link>
+
+      <a href="#">
+        <i class="fas fa-code"></i>
+        Código Fonte
+      </a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
